97c5c1914ad8fb3cafa9315bd65d73cc3adfe38c
Nicolas Rios <riosc.nicolas@gmail.com>
2017-03-21 23:38:19 -0300

    Initial Commit

---
+++ src/regex/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Envy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        String regex_name="^([A-Z][a-z ]+)+$";
+        do{
+            input = scanner.nextLine();
+            System.out.println(input.matches(regex_name));
+                    
+            
+            
+        }while(true);
+        
+    }
+    
+}
